e077fe91750ce8bf0e4c3bb6b60061e46256717c
Christian Larrea <57969291+BlackSkyFrost@users.noreply.github.com>
2025-07-18 16:40:34 -0500

    Tribulations

--- estimate.py
+++ estimate.py
@@ -2,6 +2,7 @@ import fbase
 route = r'C:\Users\gesta\Downloads\ahoy-2326b-d66bd72aa380.json'
 base = fbase.base_init(route)
 products = fbase.products_base(base)
-for el in products:
-    print(f'{el.id}=>{el.to_dict()}')
+# Comenzar desde una colección raíz
+for doc in base.collection('products').stream():
+    fbase.recorrer_documento(doc)
 base.close()
